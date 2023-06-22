a078d14901c6b438c7a030a042f5c1403472803d
4mishra <jagada.mishradev@gmail.com>
2023-06-22 21:03:10 +0530

    Functionalities to add

---
+++ bot.py
@@ -0,0 +1,13 @@
+from instadm import InstaDM
+
+
+# Auto login
+insta = InstaDM(username='', password='')
+
+insta.sendMessage(user='customer', message='price')
+# send group message to chat id
+# send group message to user
+# send message to user
+# go to page and type message
+# get username and create custom greeting
+
